e70c19be4d799ef440d0cc87fb5b1a6f77f3abc5
John Dempsey <john.dempsey@iadt.ie>
2024-11-12 17:08:08 +0000

    Initial database setup

---
+++ app/models/Post.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from datetime import datetime, timezone
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import db
+from app.models.User import User
+
+class Post(db.Model):
+    id:        so.Mapped[int] = so.mapped_column(primary_key=True)
+    body:      so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, 
+                                                      default=lambda: datetime.now(timezone.utc))
+    user_id:   so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
